e84d76a787b30b891d6a17d2ee71c90eeac7f4c1
Serge Bishyr <SeriyBg@gmail.com>
2016-07-24 16:25:40 +0300

    Add Home and About pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render () {
+    return(
+      <div>
+        <h1>About</h1>
+        <p>Learning React and Redux Application on ES6</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
