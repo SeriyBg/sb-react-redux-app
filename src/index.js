46a39d1ca1c107105649d2207418f7efaadf2035
Serge Bishyr <SeriyBg@gmail.com>
2016-08-06 15:26:41 +0300

    Add redux asynch call of data from mockApi

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import configureStore from './store/configureStore';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
